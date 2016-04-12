e79a661eac7a8d30cc11ebd9fae171c7a149fa98
Mohamed Saleem <mohsal.saleem@gmail.com>
2016-04-13 00:52:35 +0530

    Best Fit done

---
+++ instance_manager.py
@@ -0,0 +1,94 @@
+import MySQLdb
+
+class InstanceManager(object):
+	"""docstring for InstanceManager"""
+
+	def __init__(self):
+		super(InstanceManager, self).__init__()
+
+	def node_details(self):
+		node_details_array = []
+		db = MySQLdb.connect("localhost","root","password","nova")
+		cursor = db.cursor()
+		cursor.execute("select free_disk_gb,free_ram_mb,vcpus,vcpus_used,id,local_gb,memory_mb,hypervisor_hostname from compute_nodes")
+
+		data = cursor.fetchall()
+		for row in data:
+			node_details_dict = {}
+			free_disk = row[0]
+			free_ram = row[1]
+			free_vcpus = row[2] - row[3]
+			total_disk = row[5]
+			total_ram = row[6]
+			total_vcpus = row[2]
+			nodename = row[7]
+			hostname = row[7]
+			uuid = row[4]
+			
+			node_details_dict['nodename'] = nodename
+			node_details_dict['free_disk'] = free_disk
+			node_details_dict['free_ram'] = free_ram
+			node_details_dict['free_vcpus'] = free_vcpus
+			node_details_dict['total_disk'] = total_disk
+			node_details_dict['total_ram'] = total_ram
+			node_details_dict['total_vcpus'] = total_vcpus
+			node_details_dict['hostname'] = hostname
+			node_details_array.append(node_details_dict)
+
+		return node_details_array
+
+	def feasible_nodes(self, vm):
+		nodes = []
+
+		db = MySQLdb.connect("localhost","root","password","nova")
+		cursor = db.cursor()
+		query_string = 'select id,hypervisor_hostname from compute_nodes where memory_mb >= %s and vcpus >= %s and local_gb >= %s'
+		cursor.execute(query_string,(str(vm['ram']),str(vm['vcpus']),str(vm['disk'])))
+
+		data = cursor.fetchall()
+
+		for row in data:
+			node_dict = {}
+			hostname = row[1]
+			uuid = row[0]
+
+			node_dict['hostname'] = hostname
+			node_dict['uuid'] = uuid
+			nodes.append(node_dict)
+
+		return nodes
+	
+	def vm_list(self,hostname):
+		vm_list = []
+		
+		db = MySQLdb.connect("localhost","root","password","nova")
+		cursor = db.cursor()
+		query_string = 'select display_name,uuid,memory_mb,vcpus,root_gb from instances where host=%s and vm_state=%s '
+		cursor.execute(query_string,(str(vm['hostname']),'active'))
+
+		data = cursor.fetchall()
+
+		for row in data:
+			vm_data = {}
+			uuid = row[1]
+			display_name = row[0]
+			ram = row[2]
+			vcpus = row[3]
+			disk = row[4]
+
+			vm_data['ram'] = ram
+			vm_data['vcpus'] = vcpus
+			vm_data['disk'] = disk
+			vm_data['uuid'] = uuid
+			vm_data['name'] = display_name
+
+			vm_list.append(vm_data)
+		return vm_list
+
+	def live_migrate(self,migration_list):
+		for vm in migration_list:
+			vm_id = str(vm[0]['uuid'])
+			vm_name = str(vm[0]['name'])
+			hostname = str(vm[1])
+			subprocess.Popen("/opt/stack/nova/nova/scheduler/./nova_server_migration.sh %s %s" % (vm_id,hostname), shell=True)
+			LOG.debug("Migrating VM %(vm_name)s to %(node_name)s..", { 'vm_name': vm_name,'node_name': hostname })
---
+++ threshold.py
@@ -0,0 +1,170 @@
+# import time, threading
+from __future__ import division
+from nova.openstack.common import log as logging
+import MySQLdb
+import subprocess
+
+LOG = logging.getLogger(__name__)
+
+
+class ThresholdManager():
+	"""docstring for ThresholdManager"""
+
+	on_demand_high = 0
+	on_demand_low = 0
+	spot = 0	
+
+	def __init__(self):
+		self.update_attributes()
+
+	def get_vcpus_data(self):
+		db = MySQLdb.connect("localhost","root","password","nova")
+		cursor = db.cursor()
+		cursor.execute("select vcpus,vcpus_used from compute_nodes")
+		data = cursor.fetchall()
+		vcpus = 0
+		vcpus_used = 0
+		for row in data:
+			vcpus += row[0]
+			vcpus_used += row[1]	
+		LOG.debug('Virtual CPUs %(vcpus)s',{'vcpus': vcpus})
+		LOG.debug('Virtual CPUs Used %(vcpus_used)s', {'vcpus_used': vcpus_used})
+		db.close()
+		return [vcpus, vcpus_used]
+
+	def get_ram_data(self):
+		db = MySQLdb.connect("localhost", "root", "password", "nova")
+		cursor = db.cursor()
+		cursor.execute("select memory_mb,memory_mb_used from compute_nodes")
+		data = cursor.fetchall()
+		total_ram = 0
+		total_ram_used = 0
+
+		for row in data:
+			total_ram += row[0]
+			total_ram_used = row[1]
+		LOG.debug('Ram %(ram)s', {'ram': total_ram})
+		LOG.debug('Ram used %(ram_used)s', {'ram_used': total_ram_used})
+		db.close()
+		return [total_ram, total_ram_used]
+
+	def get_server_data(self):
+		db = MySQLdb.connect("localhost","root","password","nova")
+		cursor = db.cursor()
+		cursor.execute("select id from instance_types where name='tiny.spot'")
+		data = cursor.fetchall()
+		spot_instance_id = 0
+
+		for row in data:
+			spot_instance_id = row[0]
+
+		spot_instances_data = []
+		cursor.execute("select display_name,id,uuid,vm_state,instance_type_id from instances where instance_type_id='8' and vm_state='active'")
+		data = cursor.fetchall()
+
+		for row in data:
+			instance_data = {}
+			instance_data['name'] = row[0]
+			instance_data['id'] = row[1]
+			instance_data['uuid'] = row[2]
+			instance_data['vm_state'] = row[3]
+			spot_instances_data.append(instance_data)
+
+		return spot_instances_data
+
+	def get_ondemand_low_data(self):
+		on_demand_low_data = []
+		db = MySQLdb.connect("localhost","root","password","nova")
+		cursor = db.cursor()
+		cursor.execute("select display_name,id,uuid,vm_state,instance_type_id from instances where instance_type_id='7' and vm_state='active'")
+		data = cursor.fetchall()
+
+		for row in data:
+			instance_data = {}
+			instance_data['name'] = row[0]
+			instance_data['id'] = row[1]
+			instance_data['uuid'] = row[2]
+			instance_data['vm_state'] = row[3]
+			on_demand_low_data.append(instance_data)
+
+		return on_demand_low_data
+
+	def get_paused_on_demand_servers(self):
+		on_demand_low_data = []
+		db = MySQLdb.connect("localhost","root","password","nova")
+		cursor = db.cursor()
+		cursor.execute("select display_name,id,uuid,vm_state,instance_type_id from instances where instance_type_id='7' and vm_state='paused'")
+		data = cursor.fetchall()
+
+		for row in data:
+			instance_data = {}
+			instance_data['name'] = row[0]
+			instance_data['id'] = row[1]
+			instance_data['uuid'] = row[2]
+			instance_data['vm_state'] = row[3]
+			on_demand_low_data.append(instance_data)
+
+		return on_demand_low_data
+
+	def update_attributes(self):
+		vcpus_data = self.get_vcpus_data()
+		ram_data = self.get_ram_data()
+		LOG.debug('VCPUS %(vcpus)s', {'vcpus': vcpus_data})
+		vcpu_usage = vcpus_data[1]/vcpus_data[0]*100
+		ram_usage = ram_data[1]/ram_data[0]*100
+
+		total_usage = (vcpu_usage+ram_usage)/2
+		LOG.debug('Total Usage %(total_usage)s', {'total_usage': total_usage})
+
+		if total_usage < 25:
+			ThresholdManager.on_demand_high = 1
+			ThresholdManager.on_demand_low = 1
+			ThresholdManager.spot = 1
+			on_demand_low_paused_servers = self.get_paused_on_demand_servers()
+			LOG.debug('Server data Paused %(on_pause)s', {'on_pause': on_demand_low_paused_servers})
+			for i in on_demand_low_paused_servers:
+				if i['vm_state'] == 'paused':
+					server_name = i['uuid']
+					subprocess.Popen("/opt/stack/nova/nova/scheduler/./nova_unpause_server.sh %s" % (str(server_name)), shell=True)
+					LOG.debug("Unpausing %(unpaused_server)s", {'unpaused_server':i['name']})
+		elif total_usage >=25 and total_usage < 44:
+			ThresholdManager.on_demand_high = 1
+			ThresholdManager.on_demand_low = 1
+			ThresholdManager.spot = 0
+			on_demand_low_paused_servers = self.get_paused_on_demand_servers()
+			LOG.debug('Server data Paused %(on_pause)s', {'on_pause': on_demand_low_paused_servers})
+			for i in on_demand_low_paused_servers:
+				if i['vm_state'] == 'paused':
+					server_name = i['uuid']
+					subprocess.Popen("/opt/stack/nova/nova/scheduler/./nova_unpause_server.sh %s" % (str(server_name)), shell=True)
+					LOG.debug("Unpausing %(unpaused_server)s", {'unpaused_server':i['name']})
+
+		elif total_usage >=45:
+			ThresholdManager.on_demand_high = 1
+			ThresholdManager.on_demand_low = 0
+			ThresholdManager.spot = 0
+			servers_data = self.get_server_data()
+			LOG.debug('Servers Data %(servers_data)s', {'servers_data': servers_data})
+			for i in servers_data:
+			 	if i['vm_state'] == 'active':
+			 		server_name = i['uuid']
+			 		subprocess.Popen("/opt/stack/nova/nova/scheduler/./nova_delete_server.sh %s" % (str(server_name)), shell=True)
+			 		LOG.debug('Deleted Server %(name)s',{'name': i['name']})
+
+			on_demand_low_servers = self.get_ondemand_low_data()
+			LOG.debug('On Deman Low server Data %(on_d_l)s', {'on_d_l': on_demand_low_servers})
+			for i in on_demand_low_servers:
+				if i['vm_state'] == 'active':
+					server_name = i['uuid']
+					subprocess.Popen("/opt/stack/nova/nova/scheduler/./nova_pause_server.sh %s" % (str(server_name)), shell=True)
+					LOG.debug('Pausing Server %(name)s', {'name': i['name']})
+
+	def get_attributes(self):
+		attributes = {}
+		if ThresholdManager.on_demand_high == 1:
+			attributes['on_demand_high'] = 1
+		if ThresholdManager.on_demand_low == 1:
+			attributes['on_demand_low'] = 1
+		if ThresholdManager.spot == 1:
+			attributes['spot'] = 1
+		return attributes
